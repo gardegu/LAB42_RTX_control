cd794e0987991369957700ef132171a688486df7
massath <theo.massa@ensta-bretagne.org>
2023-05-04 12:06:41 +0200

    V1 node_arm

--- ROS_ws/src/ros_interface_umi_rtx/include/ros_interface_umi_rtx/node_arm.hpp
+++ ROS_ws/src/ros_interface_umi_rtx/include/ros_interface_umi_rtx/node_arm.hpp
@@ -2,10 +2,14 @@
 #define __ARM__
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.h"
+#include "std_msgs/msg/string.hpp"
+#include "umi-rtx/include/umi-drivers/armlib.h"
+#include "umi-rtx/include/umi-drivers/rtx.h"
 
 #include <chrono>
 #include <map>
+#include <iostream>
+#include <sstream>
 
 using namespace std::chrono_literals;
 using namespace std::placeholders;
@@ -20,10 +24,20 @@ public:
 private:
     void timer_callback();
     void init_interfaces();
+    void get_commands(const std_msgs::msg::String::SharedPtr msg);
+    void set_motors();
     void get_params();
+
+    string params2msg(); //Converts motors_params into a string to publish more easily
     
     std::chrono::milliseconds loop_dt_ = 40ms; // Timer of the node
     map<int,int> commands_motor; // Map which keeps the commands for each motor
+    map<int,map<int,int>> motors_params; // Keeps in memory the parameters of the motors
+
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_commands;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_params;
 };
 
 #endif
--- ROS_ws/src/ros_interface_umi_rtx/src/node_arm.cpp
+++ ROS_ws/src/ros_interface_umi_rtx/src/node_arm.cpp
@@ -4,6 +4,75 @@
 using namespace std::placeholders;
 using namespace std;
 
+void Arm_node::init_interfaces(){
+    timer_ = this->create_wall_timer(loop_dt_, std::bind(&Arm_node::timer_callback, this));
+    subscription_commands = this->create_subscription<std_msgs::msg::String>("motor_commands",10,
+        std::bind(&Arm_node::get_commands, this, _1));
+    publisher_params  = this->create_publisher<std_msgs::msg::String>("motor_params",10);
+}
+
+void Arm_node::timer_callback(){
+    // Collect the parameters and publish them
+    std_msgs::msg::String params;
+
+    params.data = params2msg();
+
+    publisher_params->publish(params);
+
+}
+
+void Arm_node::get_commands(const std_msgs::msg::String::SharedPtr msg){
+    stringstream ss(msg->data);
+    string pair;
+
+    commands_motor = {};
+
+    while (getline(ss, pair, ';')) {
+
+        istringstream iss(pair);
+        int key;
+
+        int value;
+        if ((iss >> key >> value)) {
+            commands_motor[key] = value;
+        }
+    }
+}
+
+void Arm_node::set_motors(){
+
+}
+
+void Arm_node::get_params(){
+    for (int motor=0; motor<NUMBER_OF_MOTORS; motor++){
+        motors_params[motor] = {};
+        for (int param=0; param<NUMBER_OF_DATA_CODES; param++){
+            int res = arm_read(motor,param,&(motors_params[motor][param]));
+        }
+    }
+}
+
+string Arm_node::params2msg(){
+    std::ostringstream oss;
+
+    for (const auto& item1 : motors_params) {
+        oss << item1.first << ": {";
+        for (const auto& item2 : item1.second) {
+            oss << item2.first << ":" << item2.second << "; ";
+        }
+        oss << "}; ";
+    }
+    std::string result = oss.str();
+    result = result.substr(0, result.length() - 2);
+
+    return result;
+    }
+
 int main(int argc, char *argv[]){
+    rclcpp::init(argc,argv);
+    shared_ptr<rclcpp::Node> node = make_shared<Arm_node>();
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return EXIT_SUCCESS;
 }
