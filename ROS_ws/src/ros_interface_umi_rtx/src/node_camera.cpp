097e8991f0fd53882499e28d8570012120788f7d
guillaume_garde <guillaume.garde@ensta-bretagne.org>
2023-05-08 12:01:55 +0200

    live feed with yellow objects detection

--- ROS_ws/src/ros_interface_umi_rtx/src/node_camera.cpp
+++ ROS_ws/src/ros_interface_umi_rtx/src/node_camera.cpp
@@ -16,12 +16,7 @@ void Camera::init_camera(){
 }
 
 void Camera::timer_callback(){
-    //int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    //int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-    //std::cout << frame_width << " " << frame_height << std::endl;
-
     cap.read(frame);
-    //std::cout << "new frame" << std::endl;
 
     cv::Mat hsv_img;
     cv::cvtColor(frame,hsv_img,cv::COLOR_BGR2HSV);
@@ -31,16 +26,30 @@ void Camera::timer_callback(){
 
     cv::Mat bin_hsv_img;
     cv::inRange(hsv_img, lower_bound, upper_bound, bin_hsv_img);
-    //std::cout << "image processed " << std::endl;
 
-    sensor_msgs::msg::Image::SharedPtr message = cv_bridge::CvImage(std_msgs::msg::Header(),"mono8",bin_hsv_img).toImageMsg();
+    /*sensor_msgs::msg::Image::SharedPtr message = cv_bridge::CvImage(std_msgs::msg::Header(),"mono8",bin_hsv_img).toImageMsg();
 
+    image_publisher->publish(*message);*/
+
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(bin_hsv_img, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+
+    double maxArea = 0;
+    int maxAreaIdx = -1;
+    for (int i = 0; i < contours.size(); i++)
+    {
+        double area = cv::contourArea(contours[i]);
+        if (area > maxArea)
+        {
+            maxArea = area;
+            maxAreaIdx = i;
+        }
+    }
+    cv::drawContours(frame, contours, maxAreaIdx, cv::Scalar(255, 255, 255), 2);
+
+    sensor_msgs::msg::Image::SharedPtr message = cv_bridge::CvImage(std_msgs::msg::Header(),"bgr8",frame).toImageMsg();
     image_publisher->publish(*message);
 
-    
-
-    //cv::imshow("Binary HSV camera feed", bin_hsv_img);
-    //cv::imshow("Feed",frame);
 }
 
 void Camera::test(){
