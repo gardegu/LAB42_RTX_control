1292e987965f7b36c915a5e43d3f9eb441eb6d2a
guillaume_garde <guillaume.garde@ensta-bretagne.org>
2023-06-29 15:23:03 +0200

    up

--- ROS_ws/src/ros_interface_umi_rtx/src/node_camera.cpp
+++ ROS_ws/src/ros_interface_umi_rtx/src/node_camera.cpp
@@ -159,6 +159,8 @@ void Camera::stereo_calibration(){
     std::vector<std::vector<cv::Point2f>> cornersLeft;
     std::vector<std::vector<cv::Point2f>> cornersRight;
 
+    cv::Size imageLeftSize, imageRightSize;
+
     for(int i=1;i<=5;i++){
         cv::Mat imageLeft = cv::imread("CalibrationImages/leftImage0" + std::to_string(i) + ".png", cv::IMREAD_GRAYSCALE);
         cv::Mat imageRight = cv::imread("CalibrationImages/rightImage0" + std::to_string(i) + ".png", cv::IMREAD_GRAYSCALE);
@@ -170,16 +172,16 @@ void Camera::stereo_calibration(){
             std::cout << "Error reading the calibration right image " << i << std::endl;
         }
 
-        cv::Size imageLeftSize = imageLeft.size();
-        cv::Size imageRightSize = imageRight.size();
+        imageLeftSize = imageLeft.size();
+        imageRightSize = imageRight.size();
         //std::cout << "Left image size: " << imageLeftSize << std::endl;
         //std::cout << "Right image size: " << imageRightSize << std::endl;
 
         std::vector<cv::Point2f> cornersLeftSub;
         std::vector<cv::Point2f> cornersRightSub;
 
-        bool patternFoundLeft = cv::findChessboardCorners(imageLeft, m_patternSize, cornersLeftSub, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
-        bool patternFoundRight = cv::findChessboardCorners(imageRight, m_patternSize, cornersRightSub, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
+        bool patternFoundLeft = cv::findChessboardCorners(imageLeft, cv::Size(7,5), cornersLeftSub, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
+        bool patternFoundRight = cv::findChessboardCorners(imageRight, cv::Size(7,5), cornersRightSub, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE);
         //std::cout << "Found corners on left image? " << patternFoundLeft << std::endl;
         //std::cout << "Found corners on right image? " << patternFoundRight << std::endl;
         //std::cout << "Number of corners detected in image " << i << " (left) test1: " << cornersLeftSub.size() << std::endl;
@@ -194,8 +196,8 @@ void Camera::stereo_calibration(){
 
             std::vector<cv::Point3f> objectPointsImage;
 
-            for (int y = 0; y < m_patternSize.height; y++) {
-                for (int x = 0; x < m_patternSize.width; x++) {
+            for (int y = 0; y < cv::Size(7,5).height; y++) {
+                for (int x = 0; x < cv::Size(7,5).width; x++) {
                     objectPointsImage.push_back(cv::Point3f(x * m_squareSize, y * m_squareSize, 0));
                 }
             }
