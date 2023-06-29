c8b3aabdc42bc6303248029039c540092a57307c
guillaume_garde <guillaume.garde@ensta-bretagne.org>
2023-06-29 14:16:52 +0200

    Stereo Calibration

--- ROS_ws/src/ros_interface_umi_rtx/include/ros_interface_umi_rtx/node_camera.hpp
+++ ROS_ws/src/ros_interface_umi_rtx/include/ros_interface_umi_rtx/node_camera.hpp
@@ -10,6 +10,11 @@
 #include <math.h>
 #include <iostream>
 #include <chrono>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/ximgproc.hpp>
 
 using namespace std::chrono_literals;
 using namespace std::placeholders;
@@ -26,8 +31,8 @@ private:
     void timer_callback();
     void init_interfaces();
     void init_camera();
-
     void get_angles(vector<vector<cv::Point>> &contours;);
+    void stereo_calibration();
 
     std::chrono::milliseconds loop_dt_ = 40ms;
 
@@ -38,6 +43,27 @@ private:
 
     cv::VideoCapture cap;
     cv::Mat frame;
+
+    cv::Size m_patternSize(7,5);
+    float m_squareSize(3.1);
+    double m_baseline = 63;
+    double m_focalLength = 2.8;
+
+    double m_rms_error;
+    cv::Mat m_cameraMatrixLeft;
+    cv::Mat m_distCoeffsLeft;
+    cv::Mat m_cameraMatrixRight;
+    cv::Mat m_distCoeffsRight;
+    cv::Mat m_R;
+    cv::Mat m_T;
+    cv::Mat m_E;
+    cv::Mat m_F;
+
+    cv::Mat m_R1;
+    cv::Mat m_R2;
+    cv::Mat m_P1;
+    cv::Mat m_P2;
+    cv::Mat m_Q;
 
     double m_cx;
     double m_cy;
