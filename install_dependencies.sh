b004b1ea20fc765c4329c5855a8965a59f06e8e9
massath <theo.massa@ensta-bretagne.org>
2023-07-25 13:49:58 +0200

    reupdate

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,11 +11,12 @@ echo export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH >> ~/.bashrc
 echo export PYTHONPATH=/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH >> ~/.bashrc
 echo export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH >> ~/.bashrc
 
-if not [-e /opt/ros/foxy/include/rviz_common/tool_manager.hpp ]
+cd /opt/ros/foxy/include/rviz_common
+if [-e tool_manager.hpp ]
 then 
-    cd /opt/ros/foxy/include/rviz_common
-    wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
+    sudo rm tool_manager.hpp
 fi
+wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
 
 source ~/.bashrc
 
