67e9f36881f577be5ab1cec104debd419e0edf93
massath <theo.massa@ensta-bretagne.org>
2023-07-25 14:07:33 +0200

    qdsqd

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,7 +14,7 @@ echo export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH >> ~/.bashrc
 cd /opt/ros/foxy/include/rviz_common
 if [-e ./tool_manager.hpp ]
 then 
-    rm tool_manager.hpp
+    rm ./tool_manager.hpp
 fi
 wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
 
