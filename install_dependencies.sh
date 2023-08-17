22900fb01bf4e103189f92971cb52a8e22474d41
massath <theo.massa@ensta-bretagne.org>
2023-08-17 10:37:36 +0200

    dqskj

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,7 +8,7 @@ sudo apt install ros-foxy-xacro -y
 
 # Verification that an important file is present, because it wasn't in my case
 cd /opt/ros/foxy/include/rviz_common
-if ![ -e ./tool_manager.hpp ]
+if ! [ -e ./tool_manager.hpp ]
 then 
     sudo wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
 fi
