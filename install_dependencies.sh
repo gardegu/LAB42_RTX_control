66f8c66d0aaa942e23fbe6c2585c4fc9e37d19ec
massath <theo.massa@ensta-bretagne.org>
2023-08-17 10:03:25 +0200

    corrected install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
+sudo apt update
 
 # Pinocchio for the inverse kinematics
 sudo apt install ros-foxy-pinocchio -y
 sudo apt install ros-foxy-xacro -y
 
-echo export PATH=/opt/openrobots/bin:$PATH >> ~/.bashrc
-echo export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH >> ~/.bashrc
-echo export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH >> ~/.bashrc
-echo export PYTHONPATH=/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH >> ~/.bashrc
-echo export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH >> ~/.bashrc
-
+# Verification that an important file is present, because it wasn't in my case
 cd /opt/ros/foxy/include/rviz_common
-if [ -e ./tool_manager.hpp ]
+if ![ -e ./tool_manager.hpp ]
 then 
-    rm ./tool_manager.hpp
+    sudo wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
 fi
-wget "https://raw.githubusercontent.com/ros2/rviz/foxy/rviz_common/src/rviz_common/tool_manager.hpp"
-
-source ~/.bashrc
 
